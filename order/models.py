bfa20e8ea66d374ecf6419a48034f0961788d3d3
fish <shayancyler1612@gmail.com>
2024-10-21 17:25:50 +0330

    fix order model

--- order/models.py
+++ order/models.py
@@ -1,6 +1,13 @@
 from django.db import models
 from lib.base_model import BaseModel
+from accounts.models import Customer
+from django.utils.translation import gettext_lazy as _
 
 class Order(BaseModel):
-    total_amount = models.IntegerField()
-    customer = models.ForeignKey(Customer, related_name='orders', on_delete=models.CASCADE)
+    total_amount = models.IntegerField(verbose_name=_('total amount'))
+    customer = models.ForeignKey(Customer, related_name='orders', on_delete=models.CASCADE, verbose_name=_('customer'))
+
+
+    class Meta:
+        verbose_name = _('order')
+        verbose_name_plural = _('orders')
